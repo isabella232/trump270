78bd050ac829821b8caf625b3fa84275511f2b46
jjelosua <jelosua@npr.org>
2016-06-22 15:29:05 -0400

    add labeling and state

--- www/js/election_simulator.js
+++ www/js/election_simulator.js
@@ -8,51 +8,56 @@ var ElectionSimulator = function(rslt_container, tab_container, ctrl_container, 
     _self.ctrl_enabled = ctrl_enabled;
     _self.tossUpStates = _.pluck(data, 'state');
 
-    _self.calculateOutcome = function(adjustments) {
-        var processedData = [];
-        _.each(baseData.data, function(row) {
-            var processedRow = {};
-            var projectedGOPVotes = 0;
-            var projectedDemVotes = 0;
-            var adjustedGOPVotes = 0;
-            var adjustedDemVotes = 0;
-            var adjustedOtherVotes = 0;
-
-            _.each(row.demographics, function(demographic) {
-                var adjustment = adjustments.adjustments[demographic.demographic];
-
-                var adjustedDemPct = demographic.d_pct - adjustment.pct;
-                adjustedDemPct = (adjustedDemPct < 0) ? 0 : adjustedDemPct;
-                adjustedDemPct = (adjustedDemPct > 1) ? 1 : adjustedDemPct;
-
-                var adjustedGOPPct = demographic.r_pct + adjustment.pct;
-                adjustedGOPPct = (adjustedGOPPct < 0) ? 0 : adjustedGOPPct;
-                adjustedGOPPct = (adjustedGOPPct > 1) ? 1 : adjustedGOPPct;
-
-                var adjustedTurnout = demographic.turnout + adjustment.turnout;
-                adjustedTurnout = (adjustedTurnout < 0) ? 0 : adjustedTurnout;
-                adjustedTurnout = (adjustedTurnout > 1) ? 1 : adjustedTurnout;
-
-                var adjustedVotes = demographic.eligible_voters * adjustedTurnout;
-
-                adjustedOtherVotes = Math.abs(demographic.r_pct - demographic.d_pct) * demographic.eligible_voters * demographic.turnout;
-                adjustedGOPVotes += adjustedVotes * adjustedGOPPct;
-                adjustedDemVotes += adjustedVotes * adjustedDemPct;
-            });
-
-            var adjustedTotalVotes = adjustedGOPVotes + adjustedDemVotes + adjustedOtherVotes;
-            processedRow.demPct = adjustedDemVotes / adjustedTotalVotes;
-            processedRow.gopPct = adjustedGOPVotes / adjustedTotalVotes;
-            processedRow.margin = processedRow.gopPct - processedRow.demPct;
-
-            processedRow.state = row.state;
-            processedRow.electoralVotes = row.electoral_votes;
-
-            processedData.push(processedRow);
-        });
-        return processedData;
-    }
 }
+
+
+ElectionSimulator.prototype.calculateOutcome = function(adjustments) {
+    var _self = this;
+    var processedData = [];
+    _.each(baseData.data, function(row) {
+        var processedRow = {};
+        var projectedGOPVotes = 0;
+        var projectedDemVotes = 0;
+        var adjustedGOPVotes = 0;
+        var adjustedDemVotes = 0;
+        var adjustedOtherVotes = 0;
+
+        _.each(row.demographics, function(demographic) {
+            var adjustment = adjustments.adjustments[demographic.demographic];
+
+            var adjustedDemPct = demographic.d_pct - adjustment.pct;
+            adjustedDemPct = (adjustedDemPct < 0) ? 0 : adjustedDemPct;
+            adjustedDemPct = (adjustedDemPct > 1) ? 1 : adjustedDemPct;
+
+            var adjustedGOPPct = demographic.r_pct + adjustment.pct;
+            adjustedGOPPct = (adjustedGOPPct < 0) ? 0 : adjustedGOPPct;
+            adjustedGOPPct = (adjustedGOPPct > 1) ? 1 : adjustedGOPPct;
+
+            var adjustedTurnout = demographic.turnout + adjustment.turnout;
+            adjustedTurnout = (adjustedTurnout < 0) ? 0 : adjustedTurnout;
+            adjustedTurnout = (adjustedTurnout > 1) ? 1 : adjustedTurnout;
+
+            var adjustedVotes = demographic.eligible_voters * adjustedTurnout;
+
+            adjustedOtherVotes = Math.abs(demographic.r_pct - demographic.d_pct) * demographic.eligible_voters * demographic.turnout;
+            adjustedGOPVotes += adjustedVotes * adjustedGOPPct;
+            adjustedDemVotes += adjustedVotes * adjustedDemPct;
+        });
+
+        var adjustedTotalVotes = adjustedGOPVotes + adjustedDemVotes + adjustedOtherVotes;
+        processedRow.demPct = adjustedDemVotes / adjustedTotalVotes;
+        processedRow.gopPct = adjustedGOPVotes / adjustedTotalVotes;
+        processedRow.margin = processedRow.gopPct - processedRow.demPct;
+
+        processedRow.state = row.state;
+        processedRow.state_usps = row.state_usps
+        processedRow.electoralVotes = row.electoral_votes;
+
+        processedData.push(processedRow);
+    });
+    return processedData;
+}
+
 
 ElectionSimulator.prototype.getDetails = function(outcome) {
     var _self = this;
